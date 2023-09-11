e931679beecdd9bb655c429a2aaf53efcc9e767e
Kanishta G <61877087+kanishta6@users.noreply.github.com>
2023-09-11 22:01:26 +0530

    Uploaded #290 of Top Interview 150

---
+++ Leetcode solutions/Word Pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        mapping = {}
+        for i in range(len(pattern)):
+            if pattern[i] in mapping:
+                if mapping[pattern[i]] != words[i]:
+                    return False
+            else:
+                if words[i] in mapping.values():
+                    return False
+                mapping[pattern[i]] = words[i]
+        return True
